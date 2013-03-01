a1c1c713f8dd2f9554e6ee083229e02c53fa9e5c
Shane McIntosh <shanemcintosh@acm.org>
2013-03-01 09:34:04 -0500

    Beginning to piece together top layer of the module

--- lib/WoC_classifier.rb
+++ lib/WoC_classifier.rb
@@ -1,9 +1,21 @@
 require "WoC_classifier/version"
 
 module WoCClassifier
-  class WoCClassifier
-    def self.hi
-      puts "Hello!"
+  class Extractor
+    def initialize(listfile, prefix="", numthreads=8)
+      @listfile = listfile
+      @prefix = prefix
+      @numthreads = numthreads
+    end
+
+    def extract
+      raise "SYSTEM ERROR: Method missing"
+    end
+  end
+
+  class Churn < Extractor
+    def initialize(listfile, prefix="", numthreads=8)
+      super(listfile, prefix, numthreads)
     end
   end
 end
