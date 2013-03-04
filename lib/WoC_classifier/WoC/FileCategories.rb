48646ef4c0a6db101eb530807283666aad64246f
Shane McIntosh <shanemcintosh@acm.org>
2013-03-04 15:08:45 -0500

    Revert "Small complexity improvement"
    This reverts commit 7d377ba570e096b9b9821d1436312f313f49dd32.

--- lib/WoC_classifier/WoC/FileCategories.rb
+++ lib/WoC_classifier/WoC/FileCategories.rb
@@ -36,8 +36,10 @@ class FileCategories
 
   def parseFile(fname)
     File.read(fname).each_line do |line|
+      line.strip!
+
       # Parse the line
-      projpath,commitid,author,commiter,aeml,ceml,lines,authtime,committime,commitfile,message = line.strip.split(";")
+      projpath,commitid,author,commiter,aeml,ceml,lines,authtime,committime,commitfile,message = line.split(";")
 
       commitfile = clean_file_path(commitfile)
 
