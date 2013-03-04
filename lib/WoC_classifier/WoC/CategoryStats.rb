e85edf6826af10ad4a8aa9c002ccc4099570fb25
Shane McIntosh <shanemcintosh@acm.org>
2013-03-04 12:24:14 -0500

    Fixed indentation

--- lib/WoC_classifier/WoC/CategoryStats.rb
+++ lib/WoC_classifier/WoC/CategoryStats.rb
@@ -5,194 +5,194 @@ require "set"
 require "WoC_classifier/WoC/Commit.rb"
 
 class CategoryStats
-    def initialize()
-        @files = {}
-        @commits = {}
-        @authors = Set.new
-        @lines_added = []
-        @lines_deleted = []
+  def initialize()
+    @files = {}
+    @commits = {}
+    @authors = Set.new
+    @lines_added = []
+    @lines_deleted = []
+  end
+
+  def add(commitid, fname, author, lines, timestamp)
+    @files[fname] = [] if (!@files[fname])
+    @files[fname].push(commitid)
+
+    @commits[commitid] = Commit.new(author, timestamp) if (!@commits[commitid])
+    @commits[commitid].add(fname, lines)
+
+    @authors.add(author)
+
+    added,deleted= @commits[commitid].getEditedLinesForFile(fname)
+    @lines_added.push(added.to_i)
+    @lines_deleted.push(deleted.to_i)
+  end
+
+  def filecount
+    return @files.size
+  end
+
+  def periods
+    rtn = Set.new
+    @commits.each do |cid, commit|
+      rtn.add(commit.timeperiod)
     end
 
-    def add(commitid, fname, author, lines, timestamp)
-        @files[fname] = [] if (!@files[fname])
-        @files[fname].push(commitid)
+    return rtn
+  end
 
-        @commits[commitid] = Commit.new(author, timestamp) if (!@commits[commitid])
-        @commits[commitid].add(fname, lines)
+  def myperiods(allperiods)
+    rtn = Set.new
+    myperiods = periods.sort
 
-        @authors.add(author)
-
-        added,deleted= @commits[commitid].getEditedLinesForFile(fname)
-        @lines_added.push(added.to_i)
-        @lines_deleted.push(deleted.to_i)
+    allperiods.each do |period|
+      rtn.add(period) if (period >= myperiods.first and period <= myperiods.last)   
     end
 
-    def filecount
-        return @files.size
+    return rtn
+  end
+
+  def commitsinperiod(commits, period)
+    commitset = Set.new
+    commits.each do |cid, commit|
+      commitset.add(cid) if (commit.timeperiod == period)
     end
 
-    def periods
-        rtn = Set.new
-        @commits.each do |cid, commit|
-            rtn.add(commit.timeperiod)
-        end
+    return commitset
+  end
 
-        return rtn
+  def size
+    return @commits.size
+  end
+
+  def authorsincommits(commits, cids)
+    authorset = Set.new
+
+    cids.each do |cid|
+      authorset.add(commits[cid].author)
     end
 
-    def myperiods(allperiods)
-        rtn = Set.new
-        myperiods = periods.sort
+    return authorset
+  end
 
-        allperiods.each do |period|
-            rtn.add(period) if (period >= myperiods.first and period <= myperiods.last)   
-        end
+  def filesincommits(commits, cids)
+    fileset = Set.new
 
-        return rtn
-    end
-
-    def commitsinperiod(commits, period)
-        commitset = Set.new
-        commits.each do |cid, commit|
-            commitset.add(cid) if (commit.timeperiod == period)
-        end
-
-        return commitset
-    end
-
-    def size
-        return @commits.size
-    end
-
-    def authorsincommits(commits, cids)
-        authorset = Set.new
-
-        cids.each do |cid|
-            authorset.add(commits[cid].author)
-        end
-
-        return authorset
-    end
-
-    def filesincommits(commits, cids)
-        fileset = Set.new
-
-        cids.each do |cid|
-            fileset.merge(commits[cid].files)
-        end
-
-
-        return fileset 
-    end
-
-    def print(allperiods, allcommits)
-        if (@commits.size > 0)
-            ratios = []
-            addsizes = []
-            delsizes = []
-            pset = myperiods(allperiods)
-            pset.each do |period|
-                mycmts = commitsinperiod(@commits, period) 
-                allcmts = commitsinperiod(allcommits, period)
-
-                ratios << mycmts.size.to_f / allcmts.size.to_f
-
-                addsum = 0
-                delsum = 0
-                mycmts.each{|id|
-                  addsum += @commits[id].lines(true)
-                  delsum += @commits[id].lines(false)
-                }
-                addsizes << addsum
-                delsizes << delsum
-            end
-
-            STDOUT.print "#{@commits.size},#{pset.size},#{@authors.size},#{@files.size},#{median(ratios)},#{median(@lines_added)},#{median(@lines_deleted)},#{median(addsizes)},#{median(delsizes)}"
-        else
-            STDOUT.print "0,0,0,0,0,0,0,0,0"
-        end
+    cids.each do |cid|
+      fileset.merge(commits[cid].files)
     end
 
 
-    def printPeriods(projname, tech, allperiods, allcommits)
-        myperiods(allperiods).each do |period|
-            mypcommits = commitsinperiod(@commits, period)
-            allpcommits = commitsinperiod(allcommits, period)
-            mypauthors = authorsincommits(@commits, mypcommits)
-            allpauthors = authorsincommits(allcommits, allpcommits)
-            mypfiles = filesincommits(@commits, mypcommits)
-            allpfiles = filesincommits(allcommits, allpcommits)
-            STDOUT.puts "#{projname},#{period},#{tech},#{mypcommits.size},#{allpcommits.size},#{mypfiles.size},#{allpfiles.size},#{mypauthors.size},#{allpauthors.size},#{linesmedian(mypcommits, true)},#{linesmedian(mypcommits, false)}"
-        end
+    return fileset 
+  end
+
+  def print(allperiods, allcommits)
+    if (@commits.size > 0)
+      ratios = []
+      addsizes = []
+      delsizes = []
+      pset = myperiods(allperiods)
+      pset.each do |period|
+        mycmts = commitsinperiod(@commits, period) 
+        allcmts = commitsinperiod(allcommits, period)
+
+        ratios << mycmts.size.to_f / allcmts.size.to_f
+
+        addsum = 0
+        delsum = 0
+        mycmts.each{|id|
+          addsum += @commits[id].lines(true)
+          delsum += @commits[id].lines(false)
+        }
+        addsizes << addsum
+        delsizes << delsum
+      end
+
+      STDOUT.print "#{@commits.size},#{pset.size},#{@authors.size},#{@files.size},#{median(ratios)},#{median(@lines_added)},#{median(@lines_deleted)},#{median(addsizes)},#{median(delsizes)}"
+    else
+      STDOUT.print "0,0,0,0,0,0,0,0,0"
+    end
+  end
+
+
+  def printPeriods(projname, tech, allperiods, allcommits)
+    myperiods(allperiods).each do |period|
+      mypcommits = commitsinperiod(@commits, period)
+      allpcommits = commitsinperiod(allcommits, period)
+      mypauthors = authorsincommits(@commits, mypcommits)
+      allpauthors = authorsincommits(allcommits, allpcommits)
+      mypfiles = filesincommits(@commits, mypcommits)
+      allpfiles = filesincommits(allcommits, allpcommits)
+      STDOUT.puts "#{projname},#{period},#{tech},#{mypcommits.size},#{allpcommits.size},#{mypfiles.size},#{allpfiles.size},#{mypauthors.size},#{allpauthors.size},#{linesmedian(mypcommits, true)},#{linesmedian(mypcommits, false)}"
+    end
+  end
+
+  def printPeriodicCouplingWith(categories, allperiods, projname, lang, tech)
+    myperiods(allperiods).each do |period|
+      bldcids = commitsinperiod(@commits, period)
+
+      srccids = Set.new
+      categories.each do |category|
+        srccids = srccids.union(commitsinperiod(category.commits, period))
+      end
+
+      srcbldcids = bldcids.intersection(srccids)
+
+      puts "#{projname},#{period},#{tech},#{lang},#{bldcids.size},#{srccids.size},#{srcbldcids.size}"
+    end
+  end
+
+  def median(arr)
+    sorted = arr.sort
+
+    if (arr.size > 2)
+      mid = arr.size/2
+      med = (arr.size % 2 == 0) ? (arr[mid-1] + arr[mid]) / 2 : arr[mid]
+    elsif (arr.size > 0)
+      med = arr[0]
+    else
+      med = 0
     end
 
-    def printPeriodicCouplingWith(categories, allperiods, projname, lang, tech)
-        myperiods(allperiods).each do |period|
-            bldcids = commitsinperiod(@commits, period)
+    return med
+  end
 
-            srccids = Set.new
-            categories.each do |category|
-                srccids = srccids.union(commitsinperiod(category.commits, period))
-            end
-
-            srcbldcids = bldcids.intersection(srccids)
-
-            puts "#{projname},#{period},#{tech},#{lang},#{bldcids.size},#{srccids.size},#{srcbldcids.size}"
-        end
+  def linesmedian(commitlist, add=true, inset=true)
+    lines = []
+    @commits.each do |cid, commit|
+      if ((inset and commitlist.include?(cid)) or (!inset and !commitlist.include?(cid)))
+        lines << commit.lines(add)
+      end
     end
 
-    def median(arr)
-        sorted = arr.sort
+    return median(lines)
+  end
 
-        if (arr.size > 2)
-            mid = arr.size/2
-            med = (arr.size % 2 == 0) ? (arr[mid-1] + arr[mid]) / 2 : arr[mid]
-        elsif (arr.size > 0)
-            med = arr[0]
-        else
-            med = 0
-        end
+  def churn(commitlist, add, inset=true)
+    lines = []
+    inter = commitlist.to_set.intersection(@commits.keys.to_set)
 
-        return med
+    thecommits = inset ? inter : @commits.keys.to_set - inter
+
+    churn = 0
+    thecommits.each do |cid|
+      churn += @commits[cid].lines(add)
     end
 
-    def linesmedian(commitlist, add=true, inset=true)
-        lines = []
-        @commits.each do |cid, commit|
-            if ((inset and commitlist.include?(cid)) or (!inset and !commitlist.include?(cid)))
-                lines << commit.lines(add)
-            end
-        end
+    return churn
+  end
 
-        return median(lines)
-    end
+  def firstCommitPeriod
+    return periods.sort.first
+  end
 
-    def churn(commitlist, add, inset=true)
-        lines = []
-        inter = commitlist.to_set.intersection(@commits.keys.to_set)
-        
-        thecommits = inset ? inter : @commits.keys.to_set - inter
+  def lastCommitPeriod
+    return periods.sort.last
+  end
 
-        churn = 0
-        thecommits.each do |cid|
-            churn += @commits[cid].lines(add)
-        end
+  def firstCommitDelay(realfirst, reallast)
+    return (firstCommitPeriod-realfirst)/(reallast-realfirst)
+  end
 
-        return churn
-    end
-
-    def firstCommitPeriod
-        return periods.sort.first
-    end
-
-    def lastCommitPeriod
-        return periods.sort.last
-    end
-
-    def firstCommitDelay(realfirst, reallast)
-        return (firstCommitPeriod-realfirst)/(reallast-realfirst)
-    end
-
-    attr_reader :commits
-    attr_reader :authors
+  attr_reader :commits
+  attr_reader :authors
 end
