eac138d12bf43d2ee12d01c0c316be68c2ba9087
Shane McIntosh <shanemcintosh@acm.org>
2013-12-20 12:17:56 -0500

    Push commit into module

--- lib/WoC_classifier/WoC/Commit.rb
+++ lib/WoC_classifier/WoC/Commit.rb
@@ -1,41 +1,43 @@
-class Commit
+module WoCClassifier
+  class Commit
+    attr_reader :author
+
     def initialize(author, timestamp)
-        @author = author
-        @timestamp = Time.at(timestamp.to_i)
-        @commitList = {}
+      @author = author
+      @timestamp = Time.at(timestamp.to_i)
+      @commitList = {}
     end
 
     def add(fname, lines)
-        @commitList[fname] = lines
+      @commitList[fname] = lines
     end
 
     def getEditedLinesForFile(fname)
-        @commitList[fname].split(":")
+      @commitList[fname].split(":")
     end
 
     def timeperiod
-        return @timestamp.year+(@timestamp.month.to_f - 1)/12.to_f
+      return @timestamp.year+(@timestamp.month.to_f - 1)/12.to_f
     end
 
     def files
-        return @commitList.keys
+      return @commitList.keys
     end
 
     def each_file
-        @commitList.each do |fname, lines|
-            yield fname, lines
-        end
+      @commitList.each do |fname, lines|
+        yield fname, lines
+      end
     end
 
     def lines(addlines=true)
-        rtn = 0
-        each_file do |fname, lines|
-            add,del = lines.split(":")
-            rtn += (addlines) ? add.to_i : del.to_i
-        end
+      rtn = 0
+      each_file do |fname, lines|
+        add,del = lines.split(":")
+        rtn += (addlines) ? add.to_i : del.to_i
+      end
 
-        return rtn
+      return rtn
     end
-
-    attr_reader :author
+  end
 end
